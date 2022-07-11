c5b32923f4b14cc1a4f82b97877230083b2bf723
Shuhao <loveyu5566@outlook.com>
2022-07-11 13:15:50 +0800

    MapDemo2-products

--- src/ex-0711-2/MapDemo2.js
+++ src/ex-0711-2/MapDemo2.js
@@ -2,10 +2,10 @@ import { Fragment } from 'react';
 // import { data } from './data/student';
 
 // 直接導入 JSON 檔，會自動轉為 js 的資料值
-import data from './data/student.json';
+import data from './data/products.json';
+import './MapDemo.css';
 
-
-function MapDemo() {
+function MapDemo2() {
   return (
     <>
       <h1>Map</h1>
@@ -21,15 +21,21 @@ function MapDemo() {
               <thead>
                 <tr>
                   <th scope="col">Id</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Birthday</th>
+                  <th scope="col">picture</th>
+                  <th scope="col">stock</th>
+                  <th scope="col">name</th>
+                  <th scope="col">price</th>
+                  <th scope="col">tags</th>
                 </tr>
               </thead>
               <tbody>
                 <tr key={v.id}>
                   <td>{v.id}</td>
+                  <img src={v.picture} />
+                  <td>{v.stock}</td>
                   <td>{v.name}</td>
-                  <td>{v.birth}</td>
+                  <td>{v.price}</td>
+                  <td>{v.tags}</td>
                 </tr>
               </tbody>
             </table>
@@ -40,4 +46,4 @@ function MapDemo() {
   );
 }
 
-export default MapDemo;
+export default MapDemo2;
