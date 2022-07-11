a427f69418897d04126051fa8b0cdcbed39d50d6
Shuhao <loveyu5566@outlook.com>
2022-07-11 16:22:37 +0800

    變換陣列裡物件的練習

--- back_up/App.js
+++ back_up/App.js
@@ -4,12 +4,11 @@ import MapDemo from './ex-0711-2/MapDemo';
 import Counter from './ex-0711-3/Counter';
 import MapDemo2 from './ex-0711-2/MapDemo2';
 import Menu from './ex-0711-menu/Menu';
-import Homework3 from './Homework3';
 
 function App() {
   return (
     <>
-      <Homework3 />
+      <Menu />
     </>
   );
 }
