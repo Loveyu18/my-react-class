720b7c2fc4ce74d69c5a447ca0269d5ac99068c3
Shuhao <loveyu5566@outlook.com>
2022-07-11 11:10:31 +0800

    map-Key的用法

--- src/ex-0711-2/MapDemo.js
+++ src/ex-0711-2/MapDemo.js
@@ -7,13 +7,37 @@ function MapDemo() {
       {/* map filter wihle wihleindex 都用這樣寫 */}
       <ul>
         {data.map((v, i) => {
-        {/* 只要看到 map 方法就一定要加 key */}
-        {/* 有 id 就要用 id */}
-        {/* 沒 id 用 uuid or nanoid */}
-        {/* 資料不會變動可以用索引值 i */}
-          return <li key={v.id}>{v.name}</li>;
+          {
+            /* 只要看到 map 方法就一定要加 key */
+          }
+          {
+            /* 有 id 就要用 id */
+          }
+          {
+            /* 沒 id 用 uuid or nanoid */
+          }
+          {
+            /* 資料不會變動可以用索引值 i */
+          }
+          return (
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Birthday</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr key={v.id}>
+                  <td>{v.id}</td>
+                  <td>{v.name}</td>
+                  <td>{v.birth}</td>
+                </tr>
+              </tbody>
+            </table>
+          );
         })}
-
       </ul>
     </>
   );
